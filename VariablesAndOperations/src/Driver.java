c2ba5b7ce964deaa3ee57799d4df5f9ce744c144
ConorMcR <illvoma404@gmail.com>
2026-09-08 19:21:54 -0600

    methods

---
+++ VariablesAndOperations/src/Driver.java
@@ -0,0 +1,40 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		String message;
+		char letter;
+		int x = 10;
+		float real = 9.9f;
+		double y = 99.9;
+		
+		message = "Hello World!";
+		letter = 'c';
+		
+		String fName = "Conor";
+		String lName = "McRae";
+		int hour, minute;
+		hour = 6;
+		minute = 30;
+		
+		System.out.println(fName + hour + minute);
+		System.out.println("Number of minutes since noon");
+		System.out.println(hour * 60 + minute);
+		
+		double half = 1 / 2.0;
+		System.out.println(half);
+		System.out.println("//////////////////////////////////////");
+		System.out.println(.1 * 10); 
+		System.out.println(.1 + .1 + .1 + .1 + .1 + .1 + .1 + .1 + .1 + .1);
+		//System.out.println(1/0);
+		x = 10;
+		
+//		while (true)
+//		{ 
+//			x = x * x;
+//			System.out.println(x);
+//			Thread.sleep(333);
+//		}}}
+		 
+	}
+}
